2a14614a756899d60aa42d007d39cbce126a95c0
Yosef <yosef0525@gmail.com>
2016-12-04 17:01:04 +0200

    add records table

---
+++ Android/app/src/main/java/com/example/yosef/finalproject/AllRecords.java
@@ -0,0 +1,13 @@
+package com.example.yosef.finalproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AllRecords extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_all_records);
+    }
+}
