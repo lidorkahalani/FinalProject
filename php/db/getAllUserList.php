7d570443c9c56b551331e0f63db7ee36e76e8272
Yosef <yosef0525@gmail.com>
2016-12-21 10:05:04 +0200

    fix hide passworde in sigin up order score talbe in desc

--- php/db/getAllUserList.php
+++ php/db/getAllUserList.php
@@ -4,7 +4,7 @@ $response=array();
 $query = $con->query("SELECT users.user_id,users.user_name,users.user_password, score_tabel.score
 						FROM users 
 						INNER JOIN score_tabel
-						ON users.user_id=score_tabel.user_id AND score_tabel.score>0 LIMIT 10");
+						ON users.user_id=score_tabel.user_id AND score_tabel.score>0 score_tabel.score desc LIMIT 10");
 $query->execute();
 $result = $query->fetchAll(PDO::FETCH_ASSOC);
 $response["usersList"]=$result;
