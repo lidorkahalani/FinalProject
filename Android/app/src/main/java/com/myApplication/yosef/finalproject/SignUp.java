c0b266f75322f9732212fa1c08279d04456423f0
Yosef <yosef0525@gmail.com>
2016-12-26 00:32:59 +0200

    change icon info now when sigin in success move directly to mainMenu change instrection html

--- Android/app/src/main/java/com/myApplication/yosef/finalproject/SignUp.java
+++ Android/app/src/main/java/com/myApplication/yosef/finalproject/SignUp.java
@@ -2,13 +2,17 @@ package com.myApplication.yosef.finalproject;
 
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.AsyncTask;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 import com.google.gson.Gson;
+
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import java.util.LinkedHashMap;
@@ -17,6 +21,8 @@ public class SignUp extends AppCompatActivity {
     private EditText userName;
     private EditText password;
     private EditText repassword;
+    int score;
+    int userId;
     private UsersDBHandler dbHandler;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,13 +57,15 @@ public class SignUp extends AppCompatActivity {
 
     public class signUp  extends AsyncTask<String, Void, Boolean>{
         LinkedHashMap<String,String> parms=new LinkedHashMap<>();
-
+        String user_Name;
+        String pass;
         @Override
         protected Boolean doInBackground(String... params) {
             Gson gson=new Gson();
-
-            parms.put("username",params[0]);
-            parms.put("password",params[1]);
+            user_Name=params[0];
+            pass=params[1];
+            parms.put("username",user_Name);
+            parms.put("password",pass);
 
             JSONParser json=new JSONParser();
             try {
@@ -80,10 +88,11 @@ public class SignUp extends AppCompatActivity {
         }
         protected void onPostExecute(Boolean result) {
             if(result) {
+                new logIn().execute(user_Name,pass);
                 Toast.makeText(SignUp.this, getResources().getString(R.string.user_sign_up_success), Toast.LENGTH_SHORT).show();
-                Intent myIntent = new Intent(SignUp.this, MainActivity.class);
+               /* Intent myIntent = new Intent(SignUp.this, MainActivity.class);
                 startActivity(myIntent);
-                finish();
+                finish();*/
             }
             else
                 Toast.makeText(SignUp.this, getResources().getString(R.string.user_sign_up_failed), Toast.LENGTH_SHORT).show();
@@ -93,5 +102,64 @@ public class SignUp extends AppCompatActivity {
 
 
         }
+        }
+
+    public class logIn extends AsyncTask<String, Void, Boolean> {
+
+        LinkedHashMap<String, String> parms = new LinkedHashMap<>();
+        String inputUserName ;
+        String inputPassword ;
+
+        @Override
+        protected Boolean doInBackground(String... params) {
+            inputUserName = params[0];
+            inputPassword = params[1];
+
+            //parms.put("password", inputPassword);
+            //  parms.put("username", inputUserName);
+            JSONParser json = new JSONParser();
+            parms.put("password", inputPassword);
+            parms.put("username", inputUserName);
+            try {
+                JSONObject response = json.makeHttpRequest(ServerUtils.login_url, "POST", parms);
+                if (response.getInt("succsses") == 1) {
+                    JSONArray jsonArray = response.getJSONArray("User");
+                    if (jsonArray.getJSONObject(0).getString("user_name").equals(inputUserName) &&
+                            jsonArray.getJSONObject(0).getString("user_password").equals(inputPassword)) {
+                        score = jsonArray.getJSONObject(0).getInt("score");
+                        userId = jsonArray.getJSONObject(0).getInt("user_id");
+                        return true;
+                    }
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+                return false;
+            } catch (Exception e) {
+                e.printStackTrace();
+                return false;
+            }
+            return false;
+        }
+        protected void onPostExecute(Boolean result) {
+            SharedPreferences myPref = PreferenceManager.getDefaultSharedPreferences(SignUp.this);
+
+            if (result) {
+                SharedPreferences.Editor editor = myPref.edit();
+                editor.putString("username", inputUserName);
+                editor.putString("password", inputPassword);
+                editor.putInt("score", score);
+                editor.putInt("user_id", userId);
+                editor.commit();
+                User currentPlayer = new User(inputUserName, inputPassword, score, userId);
+
+                Intent myIntent = new Intent(SignUp.this, MainMenu.class);
+                myIntent.putExtra("User", currentPlayer);
+                startActivity(myIntent);
+                finish();
+            } else
+                Toast.makeText(SignUp.this, getResources().getString(R.string.invalid_user_login), Toast.LENGTH_LONG).show();
+
+        }
+
     }
 }
